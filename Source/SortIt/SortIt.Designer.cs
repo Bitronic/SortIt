0e622972edc81431a57761a5f010f40740c5c5d9
Bitronic <b.adams@gmx.ch>
2017-04-09 01:34:39 +0200

    Started adding features
    Can select Folders where sorted items will be moved in.

---
+++ Source/SortIt/SortIt.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SortIt
+{
+    partial class SortIt
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.konfigurierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.einstellungenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picBox_showFile = new System.Windows.Forms.PictureBox();
+            this.btn_LoadFiles = new System.Windows.Forms.Button();
+            this.lbl_numberOfFoundFiles = new System.Windows.Forms.Label();
+            this.btn_SelectSortFolders = new System.Windows.Forms.Button();
+            this.panel_ShowSortFolders = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_showFile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.konfigurierenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1140, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // konfigurierenToolStripMenuItem
+            // 
+            this.konfigurierenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.einstellungenToolStripMenuItem});
+            this.konfigurierenToolStripMenuItem.Name = "konfigurierenToolStripMenuItem";
+            this.konfigurierenToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.konfigurierenToolStripMenuItem.Text = "Konfigurieren";
+            // 
+            // einstellungenToolStripMenuItem
+            // 
+            this.einstellungenToolStripMenuItem.Name = "einstellungenToolStripMenuItem";
+            this.einstellungenToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.einstellungenToolStripMenuItem.Text = "Einstellungen";
+            // 
+            // picBox_showFile
+            // 
+            this.picBox_showFile.Location = new System.Drawing.Point(446, 12);
+            this.picBox_showFile.Name = "picBox_showFile";
+            this.picBox_showFile.Size = new System.Drawing.Size(682, 758);
+            this.picBox_showFile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_showFile.TabIndex = 1;
+            this.picBox_showFile.TabStop = false;
+            // 
+            // btn_LoadFiles
+            // 
+            this.btn_LoadFiles.Location = new System.Drawing.Point(11, 77);
+            this.btn_LoadFiles.Name = "btn_LoadFiles";
+            this.btn_LoadFiles.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadFiles.TabIndex = 2;
+            this.btn_LoadFiles.Text = "Load Files";
+            this.btn_LoadFiles.UseVisualStyleBackColor = true;
+            this.btn_LoadFiles.Click += new System.EventHandler(this.btn_LoadFiles_Click);
+            // 
+            // lbl_numberOfFoundFiles
+            // 
+            this.lbl_numberOfFoundFiles.AutoSize = true;
+            this.lbl_numberOfFoundFiles.Location = new System.Drawing.Point(148, 82);
+            this.lbl_numberOfFoundFiles.Name = "lbl_numberOfFoundFiles";
+            this.lbl_numberOfFoundFiles.Size = new System.Drawing.Size(17, 13);
+            this.lbl_numberOfFoundFiles.TabIndex = 4;
+            this.lbl_numberOfFoundFiles.Text = "xx";
+            // 
+            // btn_SelectSortFolders
+            // 
+            this.btn_SelectSortFolders.Location = new System.Drawing.Point(11, 37);
+            this.btn_SelectSortFolders.Name = "btn_SelectSortFolders";
+            this.btn_SelectSortFolders.Size = new System.Drawing.Size(100, 23);
+            this.btn_SelectSortFolders.TabIndex = 5;
+            this.btn_SelectSortFolders.Text = "Sortier-Ordner auswählen";
+            this.btn_SelectSortFolders.UseVisualStyleBackColor = true;
+            this.btn_SelectSortFolders.Click += new System.EventHandler(this.btn_SelectSortFolders_Click);
+            // 
+            // panel_ShowSortFolders
+            // 
+            this.panel_ShowSortFolders.Location = new System.Drawing.Point(12, 129);
+            this.panel_ShowSortFolders.Name = "panel_ShowSortFolders";
+            this.panel_ShowSortFolders.Size = new System.Drawing.Size(410, 412);
+            this.panel_ShowSortFolders.TabIndex = 6;
+            // 
+            // SortIt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 782);
+            this.Controls.Add(this.panel_ShowSortFolders);
+            this.Controls.Add(this.btn_SelectSortFolders);
+            this.Controls.Add(this.lbl_numberOfFoundFiles);
+            this.Controls.Add(this.btn_LoadFiles);
+            this.Controls.Add(this.picBox_showFile);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SortIt";
+            this.Text = "ScanIt";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_showFile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem konfigurierenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem einstellungenToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picBox_showFile;
+        private System.Windows.Forms.Button btn_LoadFiles;
+        private System.Windows.Forms.Label lbl_numberOfFoundFiles;
+        private System.Windows.Forms.Button btn_SelectSortFolders;
+        private System.Windows.Forms.FlowLayoutPanel panel_ShowSortFolders;
+    }
+}
+
---
+++ Source/SortIt/SortIt.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace SortIt
+{
+    public partial class SortIt : Form
+    {
+        string origin_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\GitHub\SortIt\origin\";
+        string destination_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\GitHub\SortIt\Destination\";
+        List<string> filePaths = new List<string>();
+        List<string> sortFolders_Paths = new List<string>(); //paths 
+        List<string> sortFolders_Names = new List<string>();  //Names
+        List<Button> sortButtons = new List<Button>();
+        //Public properties for controls
+        public Label lbl_NumberOfFoundFiles
+        {
+            get { return lbl_numberOfFoundFiles; }
+            set { lbl_numberOfFoundFiles = value; }
+        }
+        public PictureBox picBox_ShowFile 
+        {
+            get {return picBox_showFile;}
+            set {picBox_showFile = value;}
+        }
+        public SortIt()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_LoadFiles_Click(object sender, EventArgs e)
+        {
+            filePaths = System.IO.Directory.GetFiles(origin_path).ToList();
+            lbl_NumberOfFoundFiles.Text = filePaths.Count.ToString() + " Dateien gefunden";
+            //shows files of folder
+            if (filePaths.Count > 0)    //If there are files in the origin folder
+            {
+                Image currentImage = null;
+                //foreach (string path in filePaths)
+                //{
+                //    try
+                //    {
+                //        currentImage = Image.FromFile(path);
+                //    }
+                //    catch
+                //    {
+                //        MessageBox.Show("Ungültige Datei gefunden: " + path);
+                //    }
+                //    picBox_ShowFile.BackgroundImage = currentImage;
+                //}
+                currentImage = Image.FromFile(filePaths[1]);
+                picBox_ShowFile.Image = currentImage;
+            }
+            else    //If no files detected
+            {
+                MessageBox.Show("Bitte zuerst Datein laden!");
+            }
+        }
+
+        private void btn_SelectSortFolders_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog folderBrowser = new CommonOpenFileDialog();
+            folderBrowser.IsFolderPicker = true;
+            folderBrowser.Multiselect = true;
+            CommonFileDialogResult result = folderBrowser.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                sortFolders_Paths = folderBrowser.FileNames.ToList();
+                foreach (string folder in sortFolders_Paths)
+                {
+                    sortFolders_Names.Add(folder.Remove(0, folder.LastIndexOf(@"\") + 1));    // +1 because of 0-based
+                }
+            }
+
+            //Generates buttons for all sort folders
+            foreach (string folderName in sortFolders_Names)
+            {
+                Button newButton = new Button();
+                newButton.Text = folderName;
+                newButton.Click += new System.EventHandler(MoveFile);
+                newButton.AutoSize = true;
+                sortButtons.Add(newButton);
+                panel_ShowSortFolders.Controls.Add(newButton);
+            }
+        }
+        private void MoveFile(object sender, EventArgs e)
+        {
+            MessageBox.Show((sender as Button).Text);
+            string Path = sortFolders_Paths.Find(folder => folder.Contains(@"\" + (sender as Button).Text));
+            MessageBox.Show(Path);
+        }
+    }
+}
